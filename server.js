cc564094348879422ca5da7aadc681abd4350bf9
Glitch (empty-blouse) <none>
2019-07-12 22:18:25 +0000

    🐩🍽 Checkpoint ./server.js:5261370/556

--- server.js
+++ server.js
@@ -30,3 +30,12 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+app.get('/api/whoami', (req, res) => {
+
+  res.json({
+    ipaddress: req.header('x-forwarded-for') || req.connection.remoteAddress,
+    language: req.header('Accept-Language')
+  });
+});
